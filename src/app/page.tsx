843906c65b6116512e31e55ddef8cc2929f1f598
alaaadeln7 <alaaadeln7@gmail.com>
2025-09-06 17:21:11 +0300

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center w-screen h-screen">
+      <h1 className="text-2xl font-bold">hello world</h1>
+    </main>
+  );
+}
--- src/app/privacy-policy/page.jsx
+++
@@ -1,9 +0,0 @@
-import PrivacyPolicyPage from "@/pages/PrivacyPolicyPage";
-
-export default function PrivacyPolicy() {
-  return (
-    <>
-      <PrivacyPolicyPage />
-    </>
-  );
-}
