783f18f569d665e2ca279e64e2b915b4a3f63348
alaaadeln7 <alaaadeln7@gmail.com>
2025-10-06 18:32:57 +0300

    add ignore typescript eslint errors

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -172,7 +172,10 @@ const setCookie = (
   document.cookie = cookieString;
 };
 
-const getCookie = (name: string) => {
+export const getCookie = (name: string): string | undefined => {
+  if (typeof document === "undefined") {
+    return undefined;
+  }
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(";").shift();
