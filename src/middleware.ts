564802fe9a8a0ade91d190ae50c4708d3624bdfb
alaaadeln7 <alaaadeln7@gmail.com>
2025-10-11 00:54:43 +0300

    handle middleware in root dir

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,7 @@ import { cookies } from "next/headers";
 export async function middleware(request: Request): Promise<NextResponse> {
   const token = (await cookies()).get("access_token");
   const isAuthenticated = token ? "true" : "false";
-  console.log("from middelware ", isAuthenticated);
-  console.log("from middelware token", token?.value);
+
   const response = NextResponse.next();
   response.cookies.set("isAuthenticated", isAuthenticated);
 
