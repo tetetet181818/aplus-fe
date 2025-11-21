1b1280ee58a1c722d576f4035df756dbe0a7b359
alaaadeln7 <alaaadeln7@gmail.com>
2025-11-21 23:35:09 +0200

    add changes in static data

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
---
+++ tests/HomePage.spec.ts
@@ -0,0 +1,23 @@
+// import HomePage from '@/components/pages/landing/HomePage';
+// test('render a hero section in landing page', () => {
+//     render(<HomePage />)
+// })
+
+// // 👇 you need these mocks if HomePage uses next/navigation or next/link
+// jest.mock("next/navigation", () => ({
+//     useRouter() {
+//         return {
+//             push: jest.fn(),
+//             replace: jest.fn(),
+//             refresh: jest.fn(),
+//         };
+//     },
+// }));
+
+// test("render a hero section in landing page", () => {
+//     render(<HomePage />);
+
+//     // example: check existence of some text in hero
+//     const heroText = screen.getByText(/welcome/i);
+//     expect(heroText).toBeInTheDocument();
+// });
